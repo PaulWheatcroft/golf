2397276e4c93689383a5837b9df070cfc2de91c7
Paul Wheatcroft <72826129+PaulWheatcroft@users.noreply.github.com>
2025-01-24 17:57:43 +0000

    refactor tee-off position logic to store multiple player positions in an array

--- src/commands/tee-off-position.test.js
+++ src/commands/tee-off-position.test.js
@@ -1,6 +1,8 @@
 import teeOffPosition from './tee-off-position'
 import Match from '../data-store/match'
 
+const positionalOptions = ["left", "middle", "right"]
+
 beforeEach(() => {
     Match.getInstance().data = []
 })
@@ -17,10 +19,21 @@ describe('tee-off', () => {
         const match = Match.getInstance()
         match.data.push({ teeOff: [{"name": "player1"}, {"name": "player2"}] })
         const firstPlayer = match.data[0].teeOff[0]
-        const positionalOptions = [1, 2, 3]
 
         teeOffPosition(firstPlayer, positionalOptions[0])
         expect(match.data).toHaveLength(2)
-        expect(match.data[1].teeOffPosition).toBe(1)
+        expect(match.data[1].teeOffPosition[0]).toBe("left")
+    })
+    it('the second player chooses the central position to tee off from and it is stored in the Match singleton', () => {
+        const match = Match.getInstance()
+        match.data.push({ teeOff: [{"name": "player1"}, {"name": "player2"}] })
+        const firstPlayer = match.data[0].teeOff[0]
+        const secondPlayer = match.data[0].teeOff[1]
+
+        teeOffPosition(firstPlayer, positionalOptions[0])
+        teeOffPosition(secondPlayer, positionalOptions[1])
+        console.dir(Match.getInstance(), { depth: null })
+        expect(match.data).toHaveLength(2)
+        expect(match.data[1].teeOffPosition[1]).toBe("middle")
     })
 })
--- src/commands/tee-off-position.ts
+++ src/commands/tee-off-position.ts
@@ -1,11 +1,18 @@
 import Match from '../data-store/match'
 
-export default function teeOffPosition(firstPlayer: { name: string }, position: number) {
+export default function teeOffPosition(playerOrder: number, position: number) {
     // no shot hasn't been taken
     const match = Match.getInstance()
     if (match.data.length === 0) {
         return 'Please select players first'
     }
-    match.data.push({ teeOffPosition: position })
-    return
+    if (match.data.length === 1) {
+        match.data.push({ teeOffPosition: [position] })
+        return
+    }
+    if (match.data.length === 2 && match.data[1].teeOffPosition.length === 1) {
+        // add the socond player tee off position to the array
+        match.data[1].teeOffPosition.push(position)
+        return
+    }
 }

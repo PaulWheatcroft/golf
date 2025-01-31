d3596ea8ce2971eddca96ac73b201fe150deda14
Paul Wheatcroft <72826129+PaulWheatcroft@users.noreply.github.com>
2025-01-31 19:49:47 +0000

    refactor club selection logic to ensure tee off position is selected before club choice

--- src/commands/club-selection.test.js
+++ src/commands/club-selection.test.js
@@ -1,35 +1,40 @@
-import playerSelection from './player-selection'
-import teeOffPosition from './tee-off-position'
 import clubSelection from './club-selection'
 import Match from '../data-store/match'
+import { clubs } from '../types/clubs'
 
-const positionalOptions = ["left", "middle", "right"]
 
 beforeEach(() => {
-    Match.getInstance().data = []
+    Match.getInstance().data = [
+        { id: 1, type: 'player', name: 'Vladislav' },
+        { id: 2, type: 'player', name: 'Paul' },
+        {
+            playerId: 1,
+            playerName: 'Vladislav',
+            type: 'teeOffPosition',
+            position: 'left'
+        },
+    ]
 })
 
-describe('club-selection', () => {
+describe('club-selection at tee off', () => {
     it('it should only run if player has selected a club', () => {
-        const player1 = { name: 'player1' }
-        const player2 = { name: 'player2' }
-
-        playerSelection([player1, player2])
-        teeOffPosition(player1, positionalOptions[0])
+        const match = Match.getInstance()
+        const player1 = match.data[0]
         const response = clubSelection(player1, "")
         expect(response).toBe('Please select a club')
         
     })
-    it('the player who is going to hit the ball selects a long iron', () => {
+    it('the player to tee off selects a long iron', () => {
         const match = Match.getInstance()
-        const player1 = { name: 'player1' }
-        const player2 = { name: 'player2' }
-
-        playerSelection([player1, player2])
-        teeOffPosition(player1, positionalOptions[0])
-        clubSelection(match.data[0].teeOff[0], "Long Iron")
-        console.dir(Match.getInstance(), { depth: null })
-        expect(match.data).toHaveLength(3)
-        expect(match.data[2].club[0]).toBe("Long Iron")
+        const player1 = match.data[0]
+        clubSelection(player1, clubs[4])
+        expect(match.data).toHaveLength(4)
+        expect(match.data[3].club.name).toBe("Long Iron")
+    })
+    it('a player cannot select a club if they have not teed off', () => {
+        const match = Match.getInstance()
+        const player2 = match.data[1]
+        const response = clubSelection(player2, clubs[3])
+        expect(response).toBe('You need to select a tee off position first')
     })
 })
--- src/commands/club-selection.ts
+++ src/commands/club-selection.ts
@@ -1,9 +1,15 @@
 import Match from '../data-store/match'
+import { Player } from '../types/index'
 
-export default function clubSelection(player: { name: string }, club: string) {
+export default function clubSelection(player: Player, club: string) {
     const match = Match.getInstance()
     if (!club) {
         return 'Please select a club'
     }
-    match.data.push({ club: [club] })
+    const checkPlayerTeeOffPosition = match.data.find((player: Player) => player.type === 'teeOffPosition')
+
+    if (checkPlayerTeeOffPosition.playerId !== player.id) {
+        return 'You need to select a tee off position first'
+    }
+    match.data.push({ playerId: player.id, playerName: player.name, type: 'clubeSelection', club })
 }

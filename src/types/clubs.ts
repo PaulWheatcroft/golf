9fd6e8b096ae758f545e023bcca9128c9dde040f
Paul Wheatcroft <72826129+PaulWheatcroft@users.noreply.github.com>
2025-02-17 13:48:29 +0000

    fix: correct formatting in clubs type definition for consistency

--- src/types/clubs.ts
+++ src/types/clubs.ts
@@ -38,7 +38,7 @@ export const clubs = [
         sand: true, 
         sandPower: [-2, -2, -1, -1, 0, 0, 0, 0, 1, 1, 1, 2],
         sandAccuracy: [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]
-    }
+    },
     {
         name: "Putter", 
         distance: 3, 
